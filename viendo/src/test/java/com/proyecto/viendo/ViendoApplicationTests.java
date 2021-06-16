827fbdde657604ddb5cb2e3ce7d79105be42a5e2
VanesaLana <82981827+VanesaLana@users.noreply.github.com>
2021-06-15 22:42:41 -0300

    Add files via upload

---
+++ viendo/src/test/java/com/proyecto/viendo/ViendoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.viendo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViendoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
